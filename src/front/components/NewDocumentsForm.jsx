d526f3253177eb1c391394fdabc55fc4f819ab1f
hugo9442 <hugo9442@gmail.com>
2025-07-06 10:26:39 +0000

    solved wrong url invoices

--- src/front/components/NewDocumentsForm.jsx
+++ src/front/components/NewDocumentsForm.jsx
@@ -49,7 +49,7 @@ const NewDocumentsForm = ({ apartmentId, onCancel, onSuccess, token }) => {
       }
 
       const res = await fetch( "https://sample-service-name-bnt3.onrender.com/documents/upload"
-        //`https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/documents/upload`
+       // `https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/documents/upload`
         , {
         method: "POST",
         headers: {
