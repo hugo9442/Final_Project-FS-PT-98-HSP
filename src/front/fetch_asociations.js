13a3d23c3ec964f297de95611142d523e9db8d7c
hugo9442 <hugo9442@gmail.com>
2025-07-08 11:41:41 +0000

    errores

--- src/front/fetch_asociations.js
+++ src/front/fetch_asociations.js
@@ -1,4 +1,4 @@
-c//onst Url = "https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/asociates";
+//onst Url = "https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/asociates";
 const Url ="https://sample-service-name-bnt3.onrender.com/asociates";
 
 
