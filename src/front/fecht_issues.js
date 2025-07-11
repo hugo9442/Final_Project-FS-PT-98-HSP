9649bb71276d6c47332d71ca524bcfef79258bfd
hugo9442 <hugo9442@gmail.com>
2025-07-11 11:26:54 +0000

    url

--- src/front/fecht_issues.js
+++ src/front/fecht_issues.js
@@ -1,6 +1,6 @@
 
-const Url = "https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/issues"
-//const Url ="https://sample-service-name-bnt3.onrender.com/issues";
+//const Url = "https://special-couscous-wrpgj9jx4q92v6xw-3001.app.github.dev/issues"
+const Url ="https://sample-service-name-bnt3.onrender.com/issues";
 
 
 export const Issues = {
